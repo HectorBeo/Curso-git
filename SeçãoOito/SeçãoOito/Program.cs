070865a0a037e3a236737d3f833a4e13054c2f75
Hector Beo <hectordbeo@gmail.com>
2020-04-09 22:39:35 -0300

    Nova linha de codigo adicionada

--- SeçãoOito/SeçãoOito/Program.cs
+++ SeçãoOito/SeçãoOito/Program.cs
@@ -7,6 +7,7 @@ namespace SeçãoOito
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Nova linha de código");
         }
     }
 }
